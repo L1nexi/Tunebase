c79cbe2782682ec0bf35784336186e94d3b9b4c1
L1nexi <l1nexi@outlook.com>
2025-03-29 16:41:35 +0800

    refacor:全局配置了过滤引擎

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,3 +138,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+# JSON Web Token Authentication settings    
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,41 +1,34 @@
 from .models import User, Playlist, Artist, Music, PlaylistContent
 from .serializers import UserSerializer, PlaylistSerializer, ArtistSerializer, MusicSerializer, PlaylistContentSerializer
 from rest_framework.viewsets import ModelViewSet
-from django_filters import rest_framework
-from rest_framework import filters
 
 # Create your views here.
 class ArtistViewSet(ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
     search_fields = ['name', 'description']
     filterset_fields = ['name']
 
 class MusicViewSet(ModelViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
-    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
     search_fields = ['title', 'album', 'artist__name', 'genre']
     filterset_fields = ['title', 'album', 'artist__name', 'genre']
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
     search_fields = ['username']
     filterset_fields = ['username']
 
 class PlaylistViewSet(ModelViewSet):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
-    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
     search_fields = ['name', 'description', 'user__username']
     filterset_fields = ['name', 'user']
 
 class PlaylistContentViewSet(ModelViewSet):
     queryset = PlaylistContent.objects.all()
     serializer_class = PlaylistContentSerializer
-    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
     search_fields = ['playlist__name', 'music__title', 'music__artist__name', 'music__genre', 'music__album']
     filterset_fields = ['playlist']
